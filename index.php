b25e450aac1d9eb7f11d8494a213cfe45c88574d
Jamo <develop@nighttimedev.com>
2019-05-16 11:15:40 +0200

    added RCON example

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
+<h1>Query a Server</h1>
 <form method="get" action="queryUserdata.php">
   <input type="text" placeholder="IP" name="ip"></input>
   <input type="text" placeholder="Port" name="port"></input>
+  <button type="submit">Query Server</button>
+</form>
+<h1>Exec rcon Command on Server</h1>
+<form method="get" action="rconUserdata.php">
+  <input type="text" placeholder="IP" name="ip"></input>
+  <input type="text" placeholder="Port" name="port"></input>
+  <input type="text" placeholder="Rcon Password" name="rconpw"></input>
+  <input type="text" placeholder="Command" name="command"></input>
   <button type="submit">Query Server</button>
 </form>
