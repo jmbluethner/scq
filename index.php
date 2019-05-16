81efca734780b81f2764a88e77baf4ce6a9642a0
Jamo <develop@nighttimedev.com>
2019-05-16 11:11:28 +0200

    added test scenario

---
+++ index.php
@@ -0,0 +1,5 @@
+<form method="get" action="queryUserdata.php">
+  <input type="text" placeholder="IP" name="ip"></input>
+  <input type="text" placeholder="Port" name="port"></input>
+  <button type="submit">Query Server</button>
+</form>
